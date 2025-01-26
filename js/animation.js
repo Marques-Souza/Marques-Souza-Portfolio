dbe993b8c638ac7265abb9505bd3967c8a6269bc
Marques Souza <marques.wsouza357@gmail.com>
2025-01-26 12:40:27 -0300

    Sobindo Portfolio

--- js/animation.js
+++ js/animation.js
@@ -1,102 +1,108 @@
+window.visibilid = ScrollReveal({ reset: true });
 
-window.visibilid = ScrollReveal({reset: true})
+ScrollReveal().reveal(".contact-animation-3 a", {
+  duration: 2500,
+  distance: "80px",
+  delay: 1500,
+  origin: "bottom",
+  reset: true,
+});
 
 // TITULOS DAS SESSÕES
 
-visibilid.reveal('.title', {
-    duration: 2000,
-    distance: '90px',
-     origin: 'bottom'
-})
+visibilid.reveal(".title", {
+  duration: 2500,
+  distance: "70px",
+  origin: "bottom",
+});
 
 // SESSÃO SKILLS
 
-visibilid.reveal('.cards', {
-    duration: 3000,
-    distance: '60px',
-    delay: 500,
-    origin: 'rihgt'
-})
-
+visibilid.reveal(".cards", {
+  duration: 2500,
+  distance: "60px",
+  delay: 500,
+  origin: "rihgt",
+});
 
 // SESSÃO POJECTS
 
-visibilid.reveal('.proj-1', {
-    duration: 500,
-    distance: '90px',
-    delay: 500,
-    origin: 'left'
-})
+visibilid.reveal(".proj-1", {
+  duration: 2500,
+  distance: "90px",
+  delay: 500,
+  origin: "left",
+});
 
-visibilid.reveal('.proj-3', {
-    duration: 500,
-    distance: '90px',
-    delay: 500,
-    origin: 'rihgt'
-})
+visibilid.reveal(".proj-3", {
+  duration: 2500,
+  distance: "90px",
+  delay: 500,
+  origin: "rihgt",
+});
 
+visibilid.reveal(".proj-4", {
+  duration: 2500,
+  distance: "90px",
+  delay: 1000,
+  origin: "left",
+});
 
-visibilid.reveal('.proj-4', {
-    duration: 500,
-    distance: '90px',
-    delay: 1000,
-    origin: 'left'
-})
+visibilid.reveal(".proj-6", {
+  duration: 2500,
+  distance: "90px",
+  delay: 1000,
+  origin: "rihgt",
+});
 
-visibilid.reveal('.proj-6', {
-    duration: 500,
-    distance: '90px',
-    delay: 1000,
-    origin: 'rihgt'
-})
+visibilid.reveal(".proj-2", {
+  duration: 2500,
+  distance: "90px",
+  delay: 1500,
+  origin: "top",
+});
 
+visibilid.reveal(".proj-5", {
+  duration: 2500,
+  distance: "90px",
+  delay: 1500,
+  origin: "bottom",
+});
 
-visibilid.reveal('.proj-2', {
-    duration: 500,
-    distance: '90px',
-    delay: 1500,
-    origin: 'top'
-})
+// SESSÃO CONTACT
 
-visibilid.reveal('.proj-5', {
-    duration: 500,
-    distance: '90px',
-    delay: 1500,
-    origin: 'bottom'
-})
+visibilid.reveal(".contact-animation", {
+  duration: 2500,
+  distance: "90px",
+  delay: 500,
+  origin: "left",
+});
 
+visibilid.reveal(".contact-animation-2", {
+  duration: 2500,
+  distance: "90px",
+  delay: 700,
+  origin: "right",
+});
 
-visibilid.reveal('.contact-animation', {
-    duration: 500,
-    distance: '90px',
-    delay: 500,
-    origin: 'left'
-})
+visibilid.reveal(".contact-animation-3", {
+  duration: 2500,
+  distance: "90px",
+  delay: 700,
+  origin: "bottom",
+});
 
-visibilid.reveal('.contact-animation-2', {
-    duration: 500,
-    distance: '90px',
-    delay: 900,
-    origin: 'right'
-})
+// SESSAO ABOUT
+visibilid.reveal(".about-animation", {
+  duration: 2500,
+  distance: "90px",
+  delay: 900,
+  origin: "bottom",
+});
 
-visibilid.reveal('.contact-animation-3', {
-    duration: 500,
-    distance: '90px',
-    delay: 900,
-    origin: 'bottom'
-})
-
-visibilid.reveal('.about-animation', {
-    duration: 500,
-    distance: '90px',   
-    delay: 900,
-    origin: 'bottom'
-})
-
-visibilid.reveal('.about-animation-2', {
-    duration: 500,
-    distance: '90px',   
-    delay: 900,
-    origin: 'right'
-})
+visibilid.reveal(".about-animation-2", {
+  duration: 2500,
+  distance: "90px",
+  delay: 900,
+  origin: "right",
+});
--- js/script.js
+++ js/script.js
@@ -1,38 +1,71 @@
-
 // SESSÃO DO MENU RESPONSIVO
-const menuHamburgue = document.querySelector('.menu-hamburgue');
-menuHamburgue.addEventListener('click', ()=>{
-   toggleMenu();
+const menuHamburgue = document.querySelector(".menu-hamburgue");
+menuHamburgue.addEventListener("click", () => {
+  toggleMenu();
 });
 
-function toggleMenu(){
-    const nav = document.querySelector('.nav-responsive')
-    menuHamburgue.classList.toggle('change')
+function toggleMenu() {
+  const nav = document.querySelector(".nav-responsive");
+  menuHamburgue.classList.toggle("change");
 
-    if (menuHamburgue.classList.contains('change')) {
-        nav.style.display = 'block'; 
-    }else{
-        nav.style.display = 'none'; 
-    }
+  if (menuHamburgue.classList.contains("change")) {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
 }
 
-// SESSÃO DOS CARDS 
-
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+emailjs.init("wYUdpeheU_WN-tM9U");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Verificar se todos os campos estão preenchidos
+    const name = document.querySelector('input[name="name"]').value;
+    const phone = document.querySelector('input[name="phone"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const subject = document.querySelector('input[name="subject"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
+
+    if (!name || !phone || !email || !subject || !message) {
+      // Se algum campo estiver vazio, exibe o alerta e impede o envio
+      alert("Please fill in all fields before submitting.");
+      return; // Interrompe o envio do formulário
+    }
+
+    // Envia o formulário para o EmailJS
+    emailjs.sendForm("service_9s7kcci", "template_u1689r8", this).then(
+      function (response) {
+        // Exibe o alerta de sucesso
+        alert("Message sent successfully!");
+        console.log("Success:", response);
+
+        // Reseta o formulário após o envio bem-sucedido
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        // Exibe o alerta de erro
+        alert("Failed to send message, please try again.");
+        console.error("Error:", error);
+      }
+    );
   });
-
-
